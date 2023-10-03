de05f04afb9f0d9a80d366e42ff286254525132d
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-03 20:52:10 +0530

    Update 5A.py

---
+++ 5A.py
@@ -0,0 +1,31 @@
+import re
+def isPH(numstr):
+    if len(numstr)!=12:
+        return False
+    for i in range (len(numstr)):
+        if i==3 or i==7:
+            if numstr[i]!='-':
+                return False
+        else:
+            if numstr[i].isdigit()==False:
+                return False
+    return True
+
+def chkpn(numstr):
+    ph_pt=re.compile(r'^\d{3}-\d{3}-\d{4}$')
+    if ph_pt.match(numstr):
+        return True
+    else:
+        return False
+    
+ph_num=input("enter a phone number")
+print("without using Regular Expression")
+if isPH(ph_num):
+    print ("valid phone number")
+else:
+    print('valid phone number')
+print ('using regular expression')
+if chkpn(ph_num):
+    print("Valid Phone Number")
+else:
+    print("invalid phone number")
