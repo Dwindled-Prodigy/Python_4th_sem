c83737132c88e4e0876dad2149fad77acf625a51
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-03 09:56:23 +0530

    Update 2A.py

--- 2A.py
+++ 2A.py
@@ -1,15 +1,11 @@
 def fib(n):
-    a=0
-    b=1
-    if (n<0):
-        print('invalid input')
-    elif(n==0):
-        return a
-    elif(n==1):
-        return b
-    else:
-         return fib(n-1)+fib(n-2)
-    
-n=int(input('enter number: '))
+    if n < 0:
+        return 'invalid input'
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+n = int(input('enter number: '))
 print(fib(n))
-    
