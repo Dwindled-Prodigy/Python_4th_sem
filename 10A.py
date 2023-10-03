111486bf6664cf81293a42a6004d28550dc31eef
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-03 14:21:36 +0530

    Update 10A.py

--- 10A.py
+++ 10A.py
@@ -1,18 +1,19 @@
-from PyPDF2 import PdfWriter,PdfReader
+from PyPDF2 import PdfWriter, PdfReader
 
-num=int(input("enter pages: "))
-pdf1=open('birds.pdf','rd')
-pdf2=open('birds.pdf','rd')
+num = int(input("Enter page number: "))
 
-pdf_writer=PdfWriter()
+pdf1 = open('birds.pdf', 'rb')
+pdf2 = open('birdspic.pdf', 'rb')
 
-pdf1_reader=PdfReader(pdf1)
-page=pdf1_reader.pages[num-1]
+pdf_writer = PdfWriter()
+
+pdf1_reader = PdfReader(pdf1)
+page = pdf1_reader.pages[num - 1]
 pdf_writer.add_page(page)
 
-pdf2_reader=PdfReader(pdf2)
-page=pdf2_reader.page[num-1]
+pdf2_reader = PdfReader(pdf2)
+page = pdf2_reader.pages[num - 1]
 pdf_writer.add_page(page)
 
-with open('output.pdf','wb') as output:
-    pdf_writer.write(output) 
+with open('output.pdf', 'wb') as output:
+    pdf_writer.write(output)
